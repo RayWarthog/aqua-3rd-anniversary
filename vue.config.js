f672c8bb633947a8a8b616033eeff40258235232
RayWarthog <choulei0112@hotmail.com>
2021-06-30 22:28:23 +0800

    Added embed info

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,13 @@ module.exports = {
         skipEmptyLines: true
       })
       .end()
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Thank you Coco 会長'
+        args[0].description = 'A farewell gift dedicated to Kiryu Coco, our beloved dragon of Hololive'
+        args[0].image = 'https://nextme.me/cover.jpg'
+        return args
+      })
   }
 }
